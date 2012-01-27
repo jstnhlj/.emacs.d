201c7daa59615fb9bf9f37924ef62b344cdb9ff4
fijoholj <jostein.holje@finn.no>
2012-01-27 08:42:13 +0100

    Added user

---
+++ users/fijoholj/project-mappings.el
@@ -0,0 +1,27 @@
+;; Project peculiarities: FINN Oppdrag
+(require 'oppdrag-mode)
+(add-hook 'find-file-hook
+          (lambda ()
+            (when (string-match-p "oppdrag-services" (buffer-file-name))
+              (oppdrag-mode))))
+
+;; Perspectives
+
+(defun custom-persp/oppdrag ()
+  (interactive)
+  (custom-persp "oppdrag"
+                (find-file "~/finn/code/oppdrag-services/app-main/web/src/")))
+
+(defun custom-persp/emacs ()
+  (interactive)
+  (custom-persp "emacs"
+                (find-file "~/.emacs.d/init.el")))
+
+(defun custom-persp/org ()
+  (interactive)
+  (custom-persp "org"
+                (find-file "~/Dropbox/org/")))
+
+(define-key persp-mode-map (kbd "C-x p o") 'custom-persp/oppdrag)
+(define-key persp-mode-map (kbd "C-x p e") 'custom-persp/emacs)
+(define-key persp-mode-map (kbd "C-<f6>") 'custom-persp/org)

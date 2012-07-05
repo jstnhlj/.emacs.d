d094d3a5dfe73236bf51e045fc28c5b8f4326d8c
Jostein Holje <jostein.holje@gmail.com>
2012-07-05 13:06:38 +0200

    Fixing submodules after merging upstream. Also setup of swank-js

--- users/fijoholj/init.el
+++ users/fijoholj/init.el
@@ -1,3 +1,14 @@
 ;; stuff goes here yo
 
 (setq-default js2-basic-offset 2)
+
+;; Set up slime-js
+;; To install, see https://github.com/swank-js/swank-js/wiki/Installation
+
+(eval-after-load "js2-mode"
+  '(progn
+     (require 'slime)
+     (require 'slime-js)
+     (require 'setup-slime-js)
+     (setq slime-js-browser-command "open -a \"Google Chrome\"")
+     (diminish 'slime-js-minor-mode)))

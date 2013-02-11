b686719f5238728f2a314b76efa438606b602e34
Jostein Holje <jostein.holje@gmail.com>
2013-02-11 15:22:05 +0100

    Refactored project-mappings

--- users/fijoholj/project-mappings.el
+++ users/fijoholj/project-mappings.el
@@ -1,20 +1,52 @@
 (defmacro project-specifics (name &rest body)
+  (declare (indent 1))
   `(progn
      (add-hook 'find-file-hook
-             (lambda ()
-               (when (string-match-p ,name (buffer-file-name))
-                 ,@body)))
+               (lambda ()
+                 (when (string-match-p ,name (buffer-file-name))
+                   ,@body)))
      (add-hook 'dired-after-readin-hook
-             (lambda ()
-               (when (string-match-p ,name (dired-current-directory))
-                 ,@body)))))
+               (lambda ()
+                 (when (string-match-p ,name (dired-current-directory))
+                   ,@body)))))
 
-;; Project peculiarities: FINN Oppdrag
+;; Emacs
+(defun custom-persp/emacs ()
+  (interactive)
+  (custom-persp "emacs"
+                (find-file "~/.emacs.d/init.el")))
+
+(define-key persp-mode-map (kbd "C-x p e") 'custom-persp/emacs)
+
+;; Org
+(defun custom-persp/org ()
+  (interactive)
+  (custom-persp "org"
+                (find-file "~/Dropbox/org/")))
+
+(define-key persp-mode-map (kbd "C-<f6>") 'custom-persp/org)
+
+;; Småjobber
+(defun custom-persp/smajobber ()
+  (interactive)
+  (custom-persp "smajobber"
+                (find-file "~/finn/code/smajobber/")))
+
+(define-key persp-mode-map (kbd "C-x p s") 'custom-persp/smajobber)
+
+(require 'smajobber-mode)
+(project-specifics "smajobber"
+                   (smajobber-mode))
+
+;; FINN Oppdrag
+(defun custom-persp/oppdrag ()
+  (interactive)
+  (custom-persp "oppdrag"
+                (find-file "~/finn/code/oppdrag-services/app-main/web/src/")))
+
+(define-key persp-mode-map (kbd "C-x p o") 'custom-persp/oppdrag)
+
 (require 'oppdrag-mode)
-(add-hook 'find-file-hook
-          (lambda ()
-            (when (string-match-p "oppdrag-services" (buffer-file-name))
-              (oppdrag-mode))))
 
 (project-specifics "oppdrag-services"
                    (set (make-local-variable 'slime-js-target-url) "http://local.finn.no:8080/")
@@ -23,27 +55,7 @@
                    (ffip-local-patterns "*.js" "*.jsp" "*.css" "*.org" "*.vm" "*jsTestDriver.conf" "*jawr.properties")
                    (oppdrag-mode))
 
-;; Perspectives
-
-(defun custom-persp/oppdrag ()
-  (interactive)
-  (custom-persp "oppdrag"
-                (find-file "~/finn/code/oppdrag-services/app-main/web/src/")))
-
-(defun custom-persp/emacs ()
-  (interactive)
-  (custom-persp "emacs"
-                (find-file "~/.emacs.d/init.el")))
-
-(defun custom-persp/org ()
-  (interactive)
-  (custom-persp "org"
-                (find-file "~/Dropbox/org/")))
-(defun custom-persp/tirilaarvig ()
-  (interactive)
-  (custom-persp "tirilaarvig"
-                (find-file "~/projects/tirilaarvig/app/")))
-
+;; HDO
 (defun custom-persp/hdo ()
   (interactive)
   (custom-persp "hdo"
@@ -53,8 +65,4 @@
                    (setq js2r-path-to-tests "/spec/javascript/")
                    (setq js2r-path-to-sources "/app/assets/javascripts/"))
 
-(define-key persp-mode-map (kbd "C-x p o") 'custom-persp/oppdrag)
-(define-key persp-mode-map (kbd "C-x p e") 'custom-persp/emacs)
-(define-key persp-mode-map (kbd "C-x p t") 'custom-persp/tirilaarvig)
 (define-key persp-mode-map (kbd "C-x p h") 'custom-persp/hdo)
-(define-key persp-mode-map (kbd "C-<f6>") 'custom-persp/org)

c95c026933eb23a27812222821faa24d83cfcad5
Jostein Holje <jostein.holje@gmail.com>
2013-06-04 08:28:10 +0200

    Remove slimejs

--- users/fijoholj/init.el
+++ users/fijoholj/init.el
@@ -2,16 +2,10 @@
 (setq-default js2-basic-offset 2)
 (setq js-indent-level 2)
 
-;; Set up slime-js
-;; To install, see https://github.com/swank-js/swank-js/wiki/Installation
+;; Disallow scrolling with mouse wheel
+(when window-system
+  (mouse-wheel-mode -1))
 
-(eval-after-load "js2-mode"
-  '(progn
-     (require 'slime)
-     (require 'slime-js)
-     (require 'setup-slime-js)
-     (setq slime-js-browser-command "open -a \"Google Chrome\"")
-     (diminish 'slime-js-minor-mode)))
-
-(when (memq window-system '(mac ns))
-  (exec-path-from-shell-initialize))
+;; Font size
+(define-key global-map (kbd "M-s +") 'zoom-in)
+(define-key global-map (kbd "M-s -") 'zoom-out)
